c42a8f562050c6693ddb6f4b0c9352504a8b7104
ngtrgiabao <ngtrgiabao251002@gmail.com>
2023-08-16 17:34:46 +0700

    feat: created card and dashboardcard story

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import { routes } from './routes'
+import { DashboardCard } from './screens/HomePage/components'
 
 function App() {
-    return <>{routes}</>
+    const chart = (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="60"
+            height="45"
+            viewBox="0 0 60 45"
+            fill="none"
+        >
+            <rect y="12" width="12" height="33" rx="2" fill="#FFC13A" />
+            <rect x="16" y="23" width="12" height="22" rx="2" fill="#FFC13A" />
+            <rect x="32" width="12" height="45" rx="2" fill="#FFC13A" />
+            <rect x="48" y="33" width="12" height="12" rx="2" fill="#FFC13A" />
+        </svg>
+    )
+
+    return (
+        <>
+            <DashboardCard
+                title="Doanh thu tháng hiện tại"
+                titleStyle="base"
+                price="7,350,000"
+                priceStyle="base"
+                percentStyle="percentLow"
+                desc="Doanh thu"
+                percentValue="1,29%"
+                chart={chart}
+            />
+        </>
+    )
 }
 
 export default App
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,113 +1,27 @@
-import * as React from 'react'
+import { HTMLAttributes, ReactNode } from 'react'
 import { VariantProps, tv } from 'tailwind-variants'
 
+import { cn } from '../../utils'
+
 export const CardVariants = tv({
-    base: ['rounded-[24px] flex flex-col justify-between shadow-[]'],
-    variants: {
-        size: {
-            base: 'w-[336px] h-[156px] px-[20px] py-[24px]',
-        },
-        background: {
-            base: 'bg-white',
-            danger: 'bg-red-500',
-        },
-        titleStyle: {
-            base: 'text-[14px] text-[#858D92] font-[400] font-["Raleway"] leading-[100%]',
-        },
-        priceStyle: {
-            base: 'text-[32px] text-[#02173F] font-["Nunito"] font-bold leading-normal',
-        },
-        percentStyle: {
-            percentLow:
-                'text-[16px] text-[#FFC13A] font-["Nunito"] not-italic font-[700] leading-[187.5%] -tracking-[0.5px] ms-[8px]',
-            percentHigh:
-                'text-[16px] text-[#4D81E7] font-["Nunito"] not-italic font-[700] leading-[187.5%] -tracking-[0.5px] ms-[8px]',
-        },
-    },
+    base: [
+        'block max-w-sm p-6 bg-white border border-gray-200 rounded-lg cursor-pointer',
+        'shadow-[rgba(0,0,0,0.02)_0px_1.8px_2.2px_0px,rgba(0,0,0,0.03)_0px_8.1px_10px_0px]',
+        'hover:bg-gray-100',
+    ],
 })
 
 export interface CardProps
-    extends React.HTMLAttributes<HTMLDivElement>,
+    extends HTMLAttributes<HTMLDivElement>,
         VariantProps<typeof CardVariants> {
-    size: 'base'
-    price?: string
-    content?: string
-    percentValue?: string
-    background: 'base' | 'danger'
-    titleStyle: 'base'
-    borderRadius?: 'base'
-    priceStyle?: 'base'
-    percentStyle?: 'percentLow' | 'percentHigh'
+    className?: string
+    children?: ReactNode
 }
 
-export const Card = ({
-    price,
-    title,
-    background,
-    size,
-    content,
-    percentValue,
-    titleStyle,
-    priceStyle,
-    percentStyle,
-    ...props
-}: CardProps) => {
-    const cardClasses = CardVariants({
-        background: background,
-        size: size,
-    })
-
-    const titleClasses = CardVariants({
-        titleStyle: titleStyle,
-    })
-
-    const priceClasses = CardVariants({
-        priceStyle: priceStyle,
-    })
-
-    const percentClasses = CardVariants({
-        percentStyle: percentStyle,
-    })
-
-    const chart = (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="60"
-            height="45"
-            viewBox="0 0 60 45"
-            fill="none"
-        >
-            <rect y="12" width="12" height="33" rx="2" fill="#FFC13A" />
-            <rect x="16" y="23" width="12" height="22" rx="2" fill="#FFC13A" />
-            <rect x="32" width="12" height="45" rx="2" fill="#FFC13A" />
-            <rect x="48" y="33" width="12" height="12" rx="2" fill="#FFC13A" />
-        </svg>
-    )
-
+export const Card = ({ children, className, ...props }: CardProps) => {
     return (
-        <div
-            data-card
-            className={cardClasses}
-            {...props}
-            style={{
-                boxShadow: `0px 1.79871666431427px 2.2138051986694336px 0px rgba(0, 0, 0, 0.02), 0px 8.139008522033691px 10.017241477966309px 0px rgba(0, 0, 0, 0.03)`,
-            }}
-        >
-            <span data-title className={titleClasses}>
-                {title}
-            </span>
-            <div className="flex items-center justify-between">
-                <div>
-                    <div data-price className={priceClasses}>
-                        {price}
-                    </div>
-                    <div className="flex items-center">
-                        <span className={titleClasses}>{content}</span>
-                        <span className={percentClasses}>{percentValue}</span>
-                    </div>
-                </div>
-                <div>{chart}</div>
-            </div>
+        <div data-card className={cn(CardVariants(), className)} {...props}>
+            {children}
         </div>
     )
 }
