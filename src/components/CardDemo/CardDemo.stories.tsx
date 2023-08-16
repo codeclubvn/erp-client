560a6ea2eeb44309af26c8f87dffec59684a4ec6
duy <duy211099@gmail.com>
2023-08-16 04:03:24 +0000

    feat: up storybook

--- src/components/CardDemo/CardDemo.stories.tsx
+++ src/components/CardDemo/CardDemo.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { CardDemo, CardDemoProps } from '.'
+import { Button } from '..'
 
 const meta: Meta<CardDemoProps> = {
     title: 'components/CardDemo',
@@ -17,8 +18,21 @@ export const Default: Story = {
 
 export const Custom: Story = {
     render: (props) => (
-        <div>
+        <div className="space-y-2">
             <CardDemo className="bg-red-200">Background Red</CardDemo>
+            <CardDemo className="space-y-2">
+                <h1 className="text-lg font-bold">Title</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Facere quaerat ut culpa. Optio accusantium animi dolore,
+                    voluptatum earum ipsa, molestias velit quia iste ratione
+                    adipisci. Eaque autem modi omnis neque fugiat vero quas
+                    aliquid cumque nulla maxime, inventore necessitatibus
+                    possimus harum vitae sint commodi quae quisquam. Harum,
+                    placeat? Doloremque, eaque?
+                </p>
+                <Button onClick={() => console.log('Clicked!')}>Submit</Button>
+            </CardDemo>
         </div>
     ),
 }
