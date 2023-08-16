e65c2e89d6399da589bf823cfc4a2ebef2af9330
ngtrgiabao <ngtrgiabao251002@gmail.com>
2023-08-16 09:11:15 +0700

    feat: created card

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import { Card } from './components/Card'
-// import { routes } from './routes'
+import { routes } from './routes'
 
 function App() {
-    return (
-        <div className="flex h-screen items-center justify-center">
-            <Card
-                price="7,350,000"
-                title="Doanh thu tháng hiện tại"
-                size="base"
-                background="base"
-                titleStyle="base"
-                priceStyle="base"
-                percentStyle="percentLow"
-                content="Doanh thu"
-                percentValue="1,29%"
-            />
-        </div>
-    )
+    return <>{routes}</>
 }
 
 export default App
