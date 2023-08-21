7cc1ebea21c3db0be40b1af1ac2fc568de298437
duy <duy211099@gmail.com>
2023-08-21 16:20:30 +0700

    feat: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import { routes } from './routes'
-const titles = {
-    home: 'Home',
-    about: 'About',
-    contact: 'Contact',
-    login: 'Login',
-    register: 'Register',
-    pageNotFound: 'OOPS! Page Not Found',
-}
+
 function App() {
     return <>{routes}</>
 }
